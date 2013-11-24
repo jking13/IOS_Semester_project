c6967c28dd584cc3c1c6cc3728cd7d786bf4eedc
John King <John@Johns-MacBook-Air.local>
2013-11-24 18:26:16 -0500

    Initial Commit

---
+++ ROFLStomp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ROFLStomp
+//
+//  Created by John King on 11/24/13.
+//  Copyright (c) 2013 John King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ROFLStomp/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  ROFLStomp
+//
+
+//  Copyright (c) 2013 John King. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
